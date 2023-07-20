06818c3b20f77406c802731643582fcc5c13c75a
vietnam23 <74097873+vietnam23@users.noreply.github.com>
2023-07-20 23:46:12 +0300

    Add files via upload

---
+++ Erropxvt3y56UUj90o9p/js/before.js
@@ -0,0 +1,1 @@
+window.addEventListener("beforeunload",(function(e){var n="It looks like you have been editing something. If you leave before saving, your changes will be lost.";return(e||window.event).returnValue=n,n}));
